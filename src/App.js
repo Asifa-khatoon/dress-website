50bf8828ab949760b4423d5cef0bd1c2fb8e464b
Asifa-khatoon <asifa441.xwave@gmail.com>
2023-09-26 08:49:01 -0700

    dressing website

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+
+import './App.css';
+import BlueDress from './components/blue-dress';
+import Feedback from './components/feedback';
+import Footer from './components/footer';
+import Header from './components/header';
+import Purple from './components/purple';
+import PurpleSection from './components/purple-2-section';
+
+
+
+
+
+function App() {
+  return (
+    <> 
+    <Header/>
+    <BlueDress/>
+    <PurpleSection/>
+    <Purple/>
+    <Feedback/>
+    <Footer/>
+ 
+   </>    
+  
+  );
+}
+
+export default App;
